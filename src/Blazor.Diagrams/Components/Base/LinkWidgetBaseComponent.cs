9acc5383364a03b2e828bc8851797bd0da5a81f0
zHaytam <haytam.zanid@gmail.com>
2020-05-29 23:00:29 +0100

    Add default Port & Link widgets

---
+++ src/Blazor.Diagrams/Components/Base/LinkWidgetBaseComponent.cs
@@ -0,0 +1,29 @@
+﻿using Blazor.Diagrams.Core.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace Blazor.Diagrams.Components.Base
+{
+    public class LinkWidgetBaseComponent : ComponentBase, IDisposable
+    {
+        [Parameter]
+        public Link Link { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Link.Changed += OnLinkChanged;
+        }
+
+        private void OnLinkChanged()
+        {
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            Link.Changed -= OnLinkChanged;
+        }
+    }
+}
---
+++ src/Blazor.Diagrams/Components/Base/PortWidgetBaseComponent.cs
@@ -0,0 +1,50 @@
+﻿using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace Blazor.Diagrams.Components.Base
+{
+    public class PortWidgetBaseComponent : ComponentBase, IDisposable
+    {
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+
+        [Parameter]
+        public Port Port { get; set; }
+
+        protected ElementReference element;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Port.Changed += OnPortChanged;
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            if (firstRender)
+            {
+                var offsets = await JSRuntime.GetOffset(element);
+                Port.Offset = new Point(offsets[0], offsets[1]);
+                Port.Position = new Point(Port.Position.X + Port.Offset.X, Port.Position.Y + Port.Offset.Y);
+                Port.RefreshAll();
+            }
+        }
+
+        private void OnPortChanged()
+        {
+            StateHasChanged();
+        }
+
+        public void Dispose()
+        {
+            Port.Changed -= OnPortChanged;
+        }
+    }
+}

5d7ad6966ef0a1f26884c175d3d5f7fc01f1ed47
zHaytam <haytam.zanid@gmail.com>
2020-05-29 15:15:22 +0100

    Add DiagramManager

---
+++ src/Blazor.Diagrams.Core/DiagramManager.cs
@@ -0,0 +1,36 @@
+﻿using Blazor.Diagrams.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Blazor.Diagrams.Core
+{
+    public class DiagramManager
+    {
+        private readonly List<Node> _nodes;
+
+        public event Action<Node> NodeAdded;
+        public event Action<Node> NodeRemoved;
+
+        public DiagramManager()
+        {
+            _nodes = new List<Node>();
+        }
+
+        public ReadOnlyCollection<Node> Nodes => _nodes.AsReadOnly();
+
+        public void AddNode(Node node)
+        {
+            _nodes.Add(node);
+            NodeAdded?.Invoke(node);
+        }
+
+        public void RemoveNode(Node node)
+        {
+            if (_nodes.Remove(node))
+            {
+                NodeRemoved?.Invoke(node);
+            }
+        }
+    }
+}

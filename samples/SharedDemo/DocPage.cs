ecbbfc40902c35d208824ec88eed82131bbbc2a1
zHaytam <haytam.zanid@gmail.com>
2020-06-14 11:05:42 +0100

    Add QuickStart tutorial

---
+++ samples/SharedDemo/DocPage.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace SharedDemo
+{
+    public class DocPage : ComponentBase
+    {
+        [Inject]
+        private IJSRuntime JsRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            await JsRuntime.InvokeVoidAsync("setup");
+        }
+    }
+}

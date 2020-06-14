9c69965d429efaa74aef4a833f9fc50a0898f6fa
zHaytam <haytam.zanid@gmail.com>
2020-06-14 14:20:03 +0100

    Add Events demo

---
+++ samples/SharedDemo/Demos/Events.razor.cs
@@ -0,0 +1,66 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Models;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace SharedDemo.Demos
+{
+    public class EventsComponent : ComponentBase
+    {
+        protected readonly DiagramManager diagramManager = new DiagramManager();
+        protected readonly List<string> events = new List<string>();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            RegisterEvents();
+
+            var node1 = NewNode(50, 50);
+            var node2 = NewNode(300, 300);
+            diagramManager.AddNode(node1);
+            diagramManager.AddNode(node2);
+            diagramManager.AddNode(NewNode(300, 50));
+
+            diagramManager.AddLink(node1.GetPort(PortAlignment.RIGHT), node2.GetPort(PortAlignment.LEFT));
+        }
+
+        private void RegisterEvents()
+        {
+            diagramManager.Changed += () =>
+            {
+                events.Add("Changed");
+                StateHasChanged();
+            };
+
+            diagramManager.NodeAdded += (n) => events.Add($"NodeAdded, NodeId={n.Id}");
+            diagramManager.NodeRemoved += (n) => events.Add($"NodeRemoved, NodeId={n.Id}");
+
+            diagramManager.SelectionChanged += (m, s) =>
+            {
+                events.Add($"SelectionChanged, Id={m.Id}, Type={m.GetType().Name}, Selected={s}");
+                StateHasChanged();
+            };
+
+            diagramManager.LinkAdded += (l) => events.Add($"LinkAdded, LinkId={l.Id}");
+
+            diagramManager.LinkAttached += (l) =>
+            {
+                events.Add($"LinkAttached, LinkId={l.Id}");
+                StateHasChanged();
+            };
+
+            diagramManager.LinkRemoved += (l) => events.Add($"LinkRemoved, LinkId={l.Id}");
+        }
+
+        private NodeModel NewNode(double x, double y)
+        {
+            var node = new NodeModel(new Point(x, y));
+            node.AddPort(PortAlignment.BOTTOM);
+            node.AddPort(PortAlignment.TOP);
+            node.AddPort(PortAlignment.LEFT);
+            node.AddPort(PortAlignment.RIGHT);
+            return node;
+        }
+    }
+}

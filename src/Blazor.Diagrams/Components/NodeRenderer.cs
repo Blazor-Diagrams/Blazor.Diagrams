da1beff516d71e54a8785f556da709fd9bfada84
zHaytam <haytam.zanid@gmail.com>
2020-09-13 13:34:46 +0100

    Performance: avoid re-rendering nodes if they didn't change

--- src/Blazor.Diagrams/Components/NodeRenderer.cs
+++ src/Blazor.Diagrams/Components/NodeRenderer.cs
@@ -2,16 +2,24 @@
 using Blazor.Diagrams.Core.Models;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
+using System;
 
 namespace Blazor.Diagrams.Components
 {
-    public class NodeRenderer : ComponentBase
+    public class NodeRenderer : ComponentBase, IDisposable
     {
+        private bool _reRender;
+
         [CascadingParameter(Name = "DiagramManager")]
         public DiagramManager DiagramManager { get; set; }
 
         [Parameter]
         public NodeModel Node { get; set; }
+
+        public void Dispose()
+        {
+            Node.Changed -= Node_Changed;
+        }
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
@@ -22,6 +30,29 @@ namespace Blazor.Diagrams.Components
             builder.OpenComponent(0, componentType);
             builder.AddAttribute(1, "Node", Node);
             builder.CloseComponent();
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            Node.Changed += Node_Changed;
+        }
+
+        protected override bool ShouldRender()
+        {
+            if (_reRender)
+            {
+                _reRender = false;
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Node_Changed()
+        {
+            _reRender = true;
         }
     }
 }

6ee2902483ddb98d871c8eaa8bc14ceeff212b5c
zHaytam <haytam.zanid@gmail.com>
2021-01-09 12:05:43 +0100

    feat: Custom group demo

---
+++ samples/SharedDemo/Demos/CustomGroup/CustomGroupModel.cs
@@ -0,0 +1,16 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Models;
+
+namespace SharedDemo.Demos.CustomGroup
+{
+    public class CustomGroupModel : GroupModel
+    {
+        public CustomGroupModel(DiagramManager diagramManager, NodeModel[] children, string title, byte padding = 30)
+            : base(diagramManager, children, padding)
+        {
+            Title = title;
+        }
+
+        public string Title { get; }
+    }
+}

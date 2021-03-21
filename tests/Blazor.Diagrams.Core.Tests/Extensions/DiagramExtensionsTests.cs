b98594b2ddd4a467f93b7f1122bbc3152e07d5fa
zHaytam <haytam.zanid@gmail.com>
2021-03-21 19:56:02 +0100

    tests: Extension methods

---
+++ tests/Blazor.Diagrams.Core.Tests/Extensions/DiagramExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using Blazor.Diagrams.Core.Extensions;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using Xunit;
+
+namespace Blazor.Diagrams.Core.Tests.Extensions
+{
+    public class DiagramExtensionsTests
+    {
+        [Fact]
+        public void GetBounds_ShouldReturnZeroRectangle_WhenNodesAreEmpty()
+        {
+            // Arrange
+            var nodes = new NodeModel[0];
+
+            // Act
+            var bounds = nodes.GetBounds();
+
+            // Assert
+            Assert.True(Rectangle.Zero.Equals(bounds));
+        }
+
+        [Fact]
+        public void GetBounds_ShouldReturnCorrectBounds()
+        {
+            // Arrange
+            var nodes = new NodeModel[]
+            {
+                new NodeModel
+                {
+                    Position = new Point(10, 10),
+                    Size = new Size(100, 100)
+                },
+                new NodeModel
+                {
+                    Position = new Point(200, 200),
+                    Size = new Size(100, 100)
+                },
+            };
+
+            // Act
+            var bounds = nodes.GetBounds();
+
+            // Assert
+            var expected = new Rectangle(10, 10, 300, 300);
+            Assert.True(expected.Equals(bounds));
+        }
+    }
+}
---
+++ tests/Blazor.Diagrams.Core.Tests/Extensions/DoubleExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using Blazor.Diagrams.Core.Extensions;
+
+namespace Blazor.Diagrams.Core.Tests.Extensions
+{
+    public class DoubleExtensionsTests
+    {
+        [Theory]
+        [InlineData(5, 10, 0.1, false)]
+        [InlineData(1.1, 1.2, 0.01, false)]
+        [InlineData(10, 10, 0.0001, true)]
+        [InlineData(10.35, 10.35, 0.0001, true)]
+        [InlineData(1.659, 1.660, 0.0001, false)]
+        [InlineData(1.65999, 1.65998, 0.0001, true)]
+        [InlineData(1.65999, 1.6599998, 0.0001, true)]
+        public void AlmostEqualTo(double num1, double num2, double tolerance, bool expected)
+        {
+            Assert.Equal(expected, num1.AlmostEqualTo(num2, tolerance));
+        }
+    }
+}

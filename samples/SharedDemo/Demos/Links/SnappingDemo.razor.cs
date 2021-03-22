66bd785f2997c5fe80a9230b290232777866a5df
zHaytam <haytam.zanid@gmail.com>
2021-03-22 19:15:04 +0100

    docs: Snapping links

---
+++ samples/SharedDemo/Demos/Links/SnappingDemo.razor.cs
@@ -0,0 +1,38 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace SharedDemo.Demos.Links
+{
+    public partial class SnappingDemo
+    {
+        private Diagram _diagram = new Diagram();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            LayoutData.Title = "Link Snapping";
+            LayoutData.Info = "While dragging a new link, it will try to find (and link) to the closest target within a radius.";
+            LayoutData.DataChanged();
+
+            InitializeDiagram();
+        }
+
+        private void InitializeDiagram()
+        {
+            _diagram.Options.Links.EnableSnapping = true;
+            _diagram.Nodes.Add(new[] { NewNode(50, 80), NewNode(200, 350), NewNode(400, 100) });
+        }
+
+        private NodeModel NewNode(double x, double y)
+        {
+            var node = new NodeModel(new Point(x, y));
+            node.AddPort(PortAlignment.Bottom);
+            node.AddPort(PortAlignment.Top);
+            node.AddPort(PortAlignment.Left);
+            node.AddPort(PortAlignment.Right);
+            return node;
+        }
+    }
+}

bb736de8523cdaeca4f53450ddfc873cc550eec8
zHaytam <haytam.zanid@gmail.com>
2021-03-22 23:00:13 +0100

    docs: Nodes

---
+++ samples/SharedDemo/Demos/Nodes/SvgDemo.razor.cs
@@ -0,0 +1,41 @@
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace SharedDemo.Demos.Nodes
+{
+    public partial class SvgDemo
+    {
+        private Diagram _diagram = new Diagram();
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+            LayoutData.Title = "SVG Nodes";
+            LayoutData.Info = "You can also have SVG nodes! All you need to do is to set the Layer to RenderLayer.SVG.";
+            LayoutData.DataChanged();
+
+            InitializeDiagram();
+        }
+
+        private void InitializeDiagram()
+        {
+            _diagram.Options.DefaultNodeComponent = typeof(SvgNodeWidget);
+
+            var node1 = NewNode(80, 80);
+            var node2 = NewNode(280, 150);
+            _diagram.Nodes.Add(node1);
+            _diagram.Nodes.Add(node2);
+            _diagram.Links.Add(new LinkModel(node1.GetPort(PortAlignment.Right), node2.GetPort(PortAlignment.Left)));
+        }
+
+        private NodeModel NewNode(double x, double y)
+        {
+            var node = new NodeModel(new Point(x, y), RenderLayer.SVG);
+            node.AddPort(PortAlignment.Left);
+            node.AddPort(PortAlignment.Right);
+            return node;
+        }
+    }
+}

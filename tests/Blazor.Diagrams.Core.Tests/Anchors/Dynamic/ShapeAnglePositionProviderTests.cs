507491db68db7c6f5388907032b906f3c898a0a6
Haytam Zanid <haytam.zanid@gmail.com>
2022-08-17 21:31:18 +0100

    Add unit tests for ShapeAnglePositionProvider

---
+++ tests/Blazor.Diagrams.Core.Tests/Anchors/Dynamic/ShapeAnglePositionProviderTests.cs
@@ -0,0 +1,45 @@
+using Blazor.Diagrams.Core.Anchors.Dynamic;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Blazor.Diagrams.Core.Tests.Anchors.Dynamic;
+
+public class ShapeAnglePositionProviderTests
+{
+    [Fact]
+    public void GetPosition_ShouldUseGetPointAtAngleOfShape()
+    {
+        // Arrange
+        var shapeMock = new Mock<IShape>();
+        var nodeMock = new Mock<NodeModel>(Point.Zero);
+        var provider = new ShapeAnglePositionProvider(70);
+        nodeMock.Setup(n => n.GetShape()).Returns(shapeMock.Object);
+
+        // Act
+        var position = provider.GetPosition(nodeMock.Object, null!);
+
+        // Assert
+        shapeMock.Verify(m => m.GetPointAtAngle(70), Times.Once);
+    }
+    
+    [Fact]
+    public void GetPosition_ShouldUseOffset_WhenProvided()
+    {
+        // Arrange
+        var shapeMock = new Mock<IShape>();
+        var nodeMock = new Mock<NodeModel>(Point.Zero);
+        var provider = new ShapeAnglePositionProvider(70, 5, -10);
+        nodeMock.Setup(n => n.GetShape()).Returns(shapeMock.Object);
+        shapeMock.Setup(s => s.GetPointAtAngle(70)).Returns(new Point(100, 50));
+
+        // Act
+        var position = provider.GetPosition(nodeMock.Object, null!);
+
+        // Assert
+        position.X.Should().Be(105);
+        position.Y.Should().Be(40);
+    }
+}

be75ddf5b6259746257d8bed00c0d366ffbe9931
Haytam Zanid <haytam.zanid@gmail.com>
2022-08-31 18:58:03 +0100

    Fix docs

--- src/Blazor.Diagrams.Core/Models/Base/ILinkable.cs
+++ src/Blazor.Diagrams.Core/Models/Base/ILinkable.cs
@@ -4,6 +4,7 @@ namespace Blazor.Diagrams.Core.Models.Base;
 
 public interface ILinkable
 {
+    public string Id { get; }
     public IReadOnlyList<BaseLinkModel> Links { get; }
 
     public bool CanAttachTo(ILinkable other);

56642ff7d354e6847bb872c2e38b0b8be20662fe
Renan Alvarenga <renan.alvarenga@wisetechglobal.com>
2023-12-12 09:58:07 +1100

    Added Tests

--- tests/Blazor.Diagrams.Core.Tests/Behaviors/ScrollBehaviorTests.cs
+++ tests/Blazor.Diagrams.Core.Tests/Behaviors/ScrollBehaviorTests.cs
@@ -1,43 +1,69 @@
 ﻿using Blazor.Diagrams.Core.Behaviors;
 using Blazor.Diagrams.Core.Events;
 using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
 using Xunit;
 
 namespace Blazor.Diagrams.Core.Tests.Behaviors
 {
-    public class ScrollBehaviorTests
-    {
-        [Fact]
-        public void Behavior_WhenBehaviorEnabled_ShouldScroll()
-        {
-            // Arrange
-            var diagram = new TestDiagram();
-            diagram.BehaviorOptions.DiagramWheelBehavior = diagram.GetBehavior<ScrollBehavior>();
-            diagram.SetContainer(new Rectangle(Point.Zero, new Size(100, 100)));
-            diagram.Options.Zoom.ScaleFactor = 1.05;
+	public class ScrollBehaviorTests
+	{
+		[Fact]
+		public void Behavior_WhenBehaviorEnabled_ShouldScroll()
+		{
+			// Arrange
+			var diagram = new TestDiagram();
+			diagram.BehaviorOptions.DiagramWheelBehavior = diagram.GetBehavior<ScrollBehavior>();
+			diagram.SetContainer(new Rectangle(Point.Zero, new Size(100, 100)));
+			diagram.Options.Zoom.ScaleFactor = 1.05;
 
-            // Act
-            diagram.TriggerWheel(new WheelEventArgs(100, 100, 0, 0, false, false, false, 100, 200, 0, 0));
+			// Act
+			diagram.TriggerWheel(new WheelEventArgs(100, 100, 0, 0, false, false, false, 100, 200, 0, 0));
 
-            // Assert
-            Assert.Equal(-95, diagram.Pan.X, 0);
-            Assert.Equal(-190, diagram.Pan.Y, 0);
-        }
+			// Assert
+			Assert.Equal(-95, diagram.Pan.X, 0);
+			Assert.Equal(-190, diagram.Pan.Y, 0);
+		}
 
-        [Fact]
-        public void Behavior_WhenBehaviorDisabled_ShouldNotScroll()
-        {
-            // Arrange
-            var diagram = new TestDiagram();
-            diagram.BehaviorOptions.DiagramWheelBehavior = null;
-            diagram.SetContainer(new Rectangle(Point.Zero, new Size(100, 100)));
+		[Fact]
+		public void Behavior_WhenBehaviorDisabled_ShouldNotScroll()
+		{
+			// Arrange
+			var diagram = new TestDiagram();
+			diagram.BehaviorOptions.DiagramWheelBehavior = null;
+			diagram.SetContainer(new Rectangle(Point.Zero, new Size(100, 100)));
 
-            // Act
-            diagram.TriggerWheel(new WheelEventArgs(100, 100, 0, 0, false, false, false, 100, 200, 0, 0));
+			// Act
+			diagram.TriggerWheel(new WheelEventArgs(100, 100, 0, 0, false, false, false, 100, 200, 0, 0));
 
-            // Assert
-            Assert.Equal(0, diagram.Pan.X);
-            Assert.Equal(0, diagram.Pan.Y);
-        }
-    }
+			// Assert
+			Assert.Equal(0, diagram.Pan.X);
+			Assert.Equal(0, diagram.Pan.Y);
+		}
+
+		[Fact]
+		public void NodeUpdatesWhenScrollingWhileDragging()
+		{
+			// Arrange
+			var diagram = new TestDiagram();
+			diagram.BehaviorOptions.DiagramWheelBehavior = diagram.GetBehavior<ScrollBehavior>();
+			diagram.SetContainer(new Rectangle(Point.Zero, new Size(100, 100)));
+			diagram.Options.Zoom.ScaleFactor = 1.05;
+			var node = new NodeModel(position: new Point(0, 0));
+			node.Size = new Size(100, 200);
+			diagram.Nodes.Add(node);
+
+			// Act
+			diagram.SelectModel(node, false);
+			diagram.TriggerPointerDown(node,
+			new PointerEventArgs(100, 100, 0, 0, false, false, false, 0, 0, 0, 0, 0, 0, string.Empty, true));
+			diagram.TriggerWheel(new WheelEventArgs(100, 100, 0, 0, false, false, false, 100, 200, 0, 0));
+
+			// Assert
+			Assert.Equal(-95, diagram.Pan.X, 0);
+			Assert.Equal(-190, diagram.Pan.Y, 0);
+			Assert.Equal(-190, node.Position.Y);
+		}
+
+	}
 }

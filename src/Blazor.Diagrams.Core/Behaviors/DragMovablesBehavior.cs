4d710a4a1ab16698cd22342db5e841fc0aaac45f
Renan Alvarenga <renan.alvarenga@wisetechglobal.com>
2023-12-14 10:20:46 +1100

    fixed cherry pick errors

--- src/Blazor.Diagrams.Core/Behaviors/DragMovablesBehavior.cs
+++ src/Blazor.Diagrams.Core/Behaviors/DragMovablesBehavior.cs
@@ -10,251 +10,140 @@ namespace Blazor.Diagrams.Core.Behaviors;
 
 public class DragMovablesBehavior : Behavior
 {
-<<<<<<< HEAD
-    private readonly Dictionary<MovableModel, Point> _initialPositions;
-    private double? _lastClientX;
-    private double? _lastClientY;
-    private bool _moved;
+	private readonly Dictionary<MovableModel, Point> _initialPositions;
+	private double? _lastClientX;
+	private double? _lastClientY;
+	private bool _moved;
+	private double _totalMovedX = 0;
+	private double _totalMovedY = 0;
 
-    public DragMovablesBehavior(Diagram diagram) : base(diagram)
-    {
-        _initialPositions = new Dictionary<MovableModel, Point>();
-        Diagram.PointerDown += OnPointerDown;
-        Diagram.PointerMove += OnPointerMove;
-        Diagram.PointerUp += OnPointerUp;
-    }
-
-    private void OnPointerDown(Model? model, PointerEventArgs e)
-    {
-        if (model is not MovableModel)
-            return;
-
-        _initialPositions.Clear();
-        foreach (var sm in Diagram.GetSelectedModels())
-        {
-            if (sm is not MovableModel movable || movable.Locked)
-                continue;
-
-            // Special case: groups without auto size on
-            if (sm is NodeModel node && node.Group != null && !node.Group.AutoSize)
-                continue;
-
-            var position = movable.Position;
-            if (Diagram.Options.GridSnapToCenter && movable is NodeModel n)
-            {
-                position = new Point(movable.Position.X + (n.Size?.Width ?? 0) / 2,
-                    movable.Position.Y + (n.Size?.Height ?? 0) / 2);
-            }
-
-            _initialPositions.Add(movable, position);
-        }
-
-        _lastClientX = e.ClientX;
-        _lastClientY = e.ClientY;
-        _moved = false;
-    }
-
-    private void OnPointerMove(Model? model, PointerEventArgs e)
-    {
-        if (_initialPositions.Count == 0 || _lastClientX == null || _lastClientY == null)
-            return;
-
-        _moved = true;
-        var deltaX = (e.ClientX - _lastClientX.Value) / Diagram.Zoom;
-        var deltaY = (e.ClientY - _lastClientY.Value) / Diagram.Zoom;
-
-        foreach (var (movable, initialPosition) in _initialPositions)
-        {
-            var ndx = ApplyGridSize(deltaX + initialPosition.X);
-            var ndy = ApplyGridSize(deltaY + initialPosition.Y);
-            if (Diagram.Options.GridSnapToCenter && movable is NodeModel node)
-            {
-                node.SetPosition(ndx - (node.Size?.Width ?? 0) / 2, ndy - (node.Size?.Height ?? 0) / 2);
-            }
-            else
-            {
-                movable.SetPosition(ndx, ndy);
-            }
-        }
-    }
-
-    private void OnPointerUp(Model? model, PointerEventArgs e)
-    {
-        if (_initialPositions.Count == 0)
-            return;
-
-        if (_moved)
-        {
-            foreach (var (movable, _) in _initialPositions)
-            {
-                movable.TriggerMoved();
-            }
-        }
-        
-        _initialPositions.Clear();
-        _lastClientX = null;
-        _lastClientY = null;
-    }
-
-    private double ApplyGridSize(double n)
-    {
-        if (Diagram.Options.GridSize == null)
-            return n;
-
-        var gridSize = Diagram.Options.GridSize.Value;
-        return gridSize * Math.Floor((n + gridSize / 2.0) / gridSize);
-    }
-
-    public override void Dispose()
-    {
-        _initialPositions.Clear();
-        
-        Diagram.PointerDown -= OnPointerDown;
-        Diagram.PointerMove -= OnPointerMove;
-        Diagram.PointerUp -= OnPointerUp;
-    }
-=======
-	public class DragMovablesBehavior : Behavior
+	public DragMovablesBehavior(Diagram diagram) : base(diagram)
 	{
-		private readonly Dictionary<MovableModel, Point> _initialPositions;
-		private double? _lastClientX;
-		private double? _lastClientY;
-		private bool _moved;
-		private double _totalMovedX = 0;
-		private double _totalMovedY = 0;
+		_initialPositions = new Dictionary<MovableModel, Point>();
+		Diagram.PointerDown += OnPointerDown;
+		Diagram.PointerMove += OnPointerMove;
+		Diagram.PointerUp += OnPointerUp;
+		Diagram.Wheel += OnPointerMove;
+	}
 
-		public DragMovablesBehavior(Diagram diagram) : base(diagram)
+	private void OnPointerDown(Model? model, PointerEventArgs e)
+	{
+		if (model is not MovableModel)
+			return;
+
+		_initialPositions.Clear();
+		foreach (var sm in Diagram.GetSelectedModels())
 		{
-			_initialPositions = new Dictionary<MovableModel, Point>();
-			Diagram.PointerDown += OnPointerDown;
-			Diagram.PointerMove += OnPointerMove;
-			Diagram.PointerUp += OnPointerUp;
-			Diagram.Wheel += OnPointerMove;
-		}
+			if (sm is not MovableModel movable || movable.Locked)
+				continue;
 
-		private void OnPointerDown(Model? model, PointerEventArgs e)
-		{
-			if (model is not MovableModel)
-				return;
+			// Special case: groups without auto size on
+			if (sm is NodeModel node && node.Group != null && !node.Group.AutoSize)
+				continue;
 
-			_initialPositions.Clear();
-			foreach (var sm in Diagram.GetSelectedModels())
+			var position = movable.Position;
+			if (Diagram.Options.GridSnapToCenter && movable is NodeModel n)
 			{
-				if (sm is not MovableModel movable || movable.Locked)
-					continue;
-
-				// Special case: groups without auto size on
-				if (sm is NodeModel node && node.Group != null && !node.Group.AutoSize)
-					continue;
-
-				var position = movable.Position;
-				if (Diagram.Options.GridSnapToCenter && movable is NodeModel n)
-				{
-					position = new Point(movable.Position.X + (n.Size?.Width ?? 0) / 2,
-						movable.Position.Y + (n.Size?.Height ?? 0) / 2);
-				}
-
-				_initialPositions.Add(movable, position);
+				position = new Point(movable.Position.X + (n.Size?.Width ?? 0) / 2,
+					movable.Position.Y + (n.Size?.Height ?? 0) / 2);
 			}
 
-			_lastClientX = e.ClientX;
-			_lastClientY = e.ClientY;
-			_moved = false;
+			_initialPositions.Add(movable, position);
 		}
 
-		public void OnPointerMove(Model? model, PointerEventArgs e)
+		_lastClientX = e.ClientX;
+		_lastClientY = e.ClientY;
+		_moved = false;
+	}
+
+	public void OnPointerMove(Model? model, PointerEventArgs e)
+	{
+		if (_initialPositions.Count == 0 || _lastClientX == null || _lastClientY == null)
+			return;
+
+		_moved = true;
+
+		var deltaX = (e.ClientX - _lastClientX.Value) / Diagram.Zoom;
+		var deltaY = (e.ClientY - _lastClientY.Value) / Diagram.Zoom;
+
+		_totalMovedX += deltaX;
+		_totalMovedY += deltaY;
+
+		moveNodes(model, _totalMovedX, _totalMovedY);
+
+		_lastClientX = e.ClientX;
+		_lastClientY = e.ClientY;
+
+	}
+
+	public void OnPointerMove(WheelEventArgs e)
+	{
+		if (_initialPositions.Count == 0 || _lastClientX == null || _lastClientY == null)
+			return;
+
+		_moved = true;
+
+		_totalMovedX += e.DeltaX;
+		_totalMovedY += e.DeltaY;
+
+		moveNodes(null, _totalMovedX, _totalMovedY);
+	}
+
+	private void moveNodes(Model? model, double deltaX, double deltaY)
+	{
+		foreach (var (movable, initialPosition) in _initialPositions)
 		{
-			if (_initialPositions.Count == 0 || _lastClientX == null || _lastClientY == null)
-				return;
+			var ndx = ApplyGridSize(deltaX + initialPosition.X);
+			var ndy = ApplyGridSize(deltaY + initialPosition.Y);
 
-			_moved = true;
-
-			var deltaX = (e.ClientX - _lastClientX.Value) / Diagram.Zoom;
-			var deltaY = (e.ClientY - _lastClientY.Value) / Diagram.Zoom;
-
-			_totalMovedX += deltaX;
-			_totalMovedY += deltaY;
-
-			moveNodes(model, _totalMovedX, _totalMovedY);
-
-			_lastClientX = e.ClientX;
-			_lastClientY = e.ClientY;
-
-		}
-
-		public void OnPointerMove(WheelEventArgs e)
-		{
-			if (_initialPositions.Count == 0 || _lastClientX == null || _lastClientY == null)
-				return;
-
-			_moved = true;
-
-			_totalMovedX += e.DeltaX;
-			_totalMovedY += e.DeltaY;
-
-			moveNodes(null, _totalMovedX, _totalMovedY);
-		}
-
-		private void moveNodes(Model? model, double deltaX, double deltaY)
-		{
-			foreach (var (movable, initialPosition) in _initialPositions)
+			if (Diagram.Options.GridSnapToCenter && movable is NodeModel node)
 			{
-				var ndx = ApplyGridSize(deltaX + initialPosition.X);
-				var ndy = ApplyGridSize(deltaY + initialPosition.Y);
-
-				if (Diagram.Options.GridSnapToCenter && movable is NodeModel node)
-				{
-					node.SetPosition(ndx - (node.Size?.Width ?? 0) / 2, ndy - (node.Size?.Height ?? 0) / 2);
-				}
-				else
-				{
-					movable.SetPosition(ndx, ndy);
-				}
+				node.SetPosition(ndx - (node.Size?.Width ?? 0) / 2, ndy - (node.Size?.Height ?? 0) / 2);
 			}
-		}
-
-		private void OnPointerUp(Model? model, PointerEventArgs e)
-		{
-			if (_initialPositions.Count == 0)
-				return;
-
-			if (_moved)
+			else
 			{
-				foreach (var (movable, _) in _initialPositions)
-				{
-					movable.TriggerMoved();
-				}
+				movable.SetPosition(ndx, ndy);
 			}
-
-			_initialPositions.Clear();
-			_totalMovedX = 0;
-			_totalMovedY = 0;
-			_lastClientX = null;
-			_lastClientY = null;
-		}
-
-		private double ApplyGridSize(double n)
-		{
-			if (Diagram.Options.GridSize == null)
-				return n;
-
-			var gridSize = Diagram.Options.GridSize.Value;
-
-			// 20 * floor((100 + 10) / 20) = 20 * 5 = 100
-			// 20 * floor((105 + 10) / 20) = 20 * 5 = 100
-			// 20 * floor((110 + 10) / 20) = 20 * 6 = 120
-			return gridSize * Math.Floor((n + gridSize / 2.0) / gridSize);
-		}
-
-		public override void Dispose()
-		{
-			_initialPositions.Clear();
-
-			Diagram.PointerDown -= OnPointerDown;
-			Diagram.PointerMove -= OnPointerMove;
-			Diagram.PointerUp -= OnPointerUp;
-			Diagram.Wheel -= OnPointerMove;
 		}
 	}
->>>>>>> 7bf2db0 (First commit)
+
+	private void OnPointerUp(Model? model, PointerEventArgs e)
+	{
+		if (_initialPositions.Count == 0)
+			return;
+
+		if (_moved)
+		{
+			foreach (var (movable, _) in _initialPositions)
+			{
+				movable.TriggerMoved();
+			}
+		}
+
+		_initialPositions.Clear();
+		_totalMovedX = 0;
+		_totalMovedY = 0;
+		_lastClientX = null;
+		_lastClientY = null;
+	}
+
+	private double ApplyGridSize(double n)
+	{
+		if (Diagram.Options.GridSize == null)
+			return n;
+
+		var gridSize = Diagram.Options.GridSize.Value;
+
+		return gridSize * Math.Floor((n + gridSize / 2.0) / gridSize);
+	}
+
+	public override void Dispose()
+	{
+		_initialPositions.Clear();
+
+		Diagram.PointerDown -= OnPointerDown;
+		Diagram.PointerMove -= OnPointerMove;
+		Diagram.PointerUp -= OnPointerUp;
+		Diagram.Wheel -= OnPointerMove;
+	}
 }

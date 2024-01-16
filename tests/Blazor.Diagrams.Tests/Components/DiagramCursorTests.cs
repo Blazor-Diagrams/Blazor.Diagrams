ccbdc3a8cff763d90cb0a0c8aab0d3daa172ae49
Shams Azam <mohammadshams.azam@wisetechglobal.com>
2024-01-16 12:29:00 +0530

    Introduced cursor change based on allow panning.

---
+++ tests/Blazor.Diagrams.Tests/Components/DiagramCursorTests.cs
@@ -0,0 +1,57 @@
+﻿using AngleSharp.Css.Dom;
+using AngleSharp.Dom;
+using Blazor.Diagrams.Components;
+using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Geometry;
+using Bunit;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Blazor.Diagrams.Core.Tests.Behaviors
+{
+    public class DiagramCursorTests
+    {
+        [Fact]
+        public void Behavior_WhenPanningOptionIsAllowed_CursorShouldBeGrab()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var diagram = new BlazorDiagram();
+            diagram.Options.AllowPanning = true;
+            ctx.JSInterop.Setup<Rectangle>("ZBlazorDiagrams.getBoundingClientRect", _ => true);
+
+            // Act
+            var cut = ctx.RenderComponent<DiagramCanvas>(parameters => parameters
+            .Add(n => n.BlazorDiagram, diagram));
+            var diagramCanvas = cut.Find(".diagram-canvas");
+            var canvasStyle = diagramCanvas.GetStyle().CssText;
+
+            // Assert
+            canvasStyle.Should().Contain("cursor: grab");
+        }
+
+        [Fact]
+        public void Behavior_WhenPanningOptionIsNotAllowed_CursorShouldBeDefault()
+        {
+            // Arrange
+            using var ctx = new TestContext();
+            var diagram = new BlazorDiagram();
+            diagram.Options.AllowPanning = false;
+            ctx.JSInterop.Setup<Rectangle>("ZBlazorDiagrams.getBoundingClientRect", _ => true);
+
+            // Act
+            var cut = ctx.RenderComponent<DiagramCanvas>(parameters => parameters
+            .Add(n => n.BlazorDiagram, diagram));
+            var diagramCanvas = cut.Find(".diagram-canvas");
+            var canvasStyle = diagramCanvas.GetStyle().CssText;
+
+            // Assert
+            canvasStyle.Should().Contain("cursor: default");
+        }
+    }
+}

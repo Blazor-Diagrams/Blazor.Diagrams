29bc9b458ae22563fc98dce08abba6c8bb3dbf1e
Suraj <suraj.desai@wisetechglobal.com>
2024-01-29 15:05:17 +0530

    fix indenting

--- tests/Blazor.Diagrams.Core.Tests/Controls/ControlsContainerTests.cs
+++ tests/Blazor.Diagrams.Core.Tests/Controls/ControlsContainerTests.cs
@@ -7,22 +7,6 @@ namespace Blazor.Diagrams.Core.Tests.Controls
 {
 	public class ControlsContainerTests
 	{
-		[Fact]
-		public void AlwaysOnControlType_AlwaysVisible()
-		{
-			// Arrange
-			var diagram = new TestDiagram();
-			var node = diagram.Nodes.Add(new NodeModel());
-			var controls = diagram.Controls.AddFor(node, ControlsType.AlwaysOn);
 
-			// Assert
-			Assert.True(controls.Visible);
-
-			node.Selected = true;
-			Assert.True(controls.Visible);
-
-			diagram.TriggerPointerEnter(node, new PointerEventArgs(0, 0, 0, 0, false, false, false, 0, 0, 0, 0, 0, 0, string.Empty, true));
-			Assert.True(controls.Visible);
-		}
 	}
 }

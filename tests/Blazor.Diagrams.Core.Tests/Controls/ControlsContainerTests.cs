48343e37d81a853ff2f836ef20ae578c352c5c8e
Suraj <suraj.desai@wisetechglobal.com>
2024-01-29 15:06:10 +0530

    fix indenting

--- tests/Blazor.Diagrams.Core.Tests/Controls/ControlsContainerTests.cs
+++ tests/Blazor.Diagrams.Core.Tests/Controls/ControlsContainerTests.cs
@@ -1,9 +1,4 @@
-﻿using Blazor.Diagrams.Core.Controls;
-using Blazor.Diagrams.Core.Events;
-using Blazor.Diagrams.Core.Models;
-using Xunit;
-
-namespace Blazor.Diagrams.Core.Tests.Controls
+﻿namespace Blazor.Diagrams.Core.Tests.Controls
 {
 	public class ControlsContainerTests
 	{

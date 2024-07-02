a7ce3eb0f6dcf3b09aed1995e612f02eebfd9eb0
Heather Cox <Heather.Cox@wisetechglobal.com>
2024-07-03 09:46:26 +1000

    refactor ResizerProvider interface to abstract class

---
+++ src/Blazor.Diagrams.Core/Positions/Resizing/ResizerProvider.cs
@@ -0,0 +1,104 @@
+﻿using Blazor.Diagrams.Core.Events;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using Blazor.Diagrams.Core.Models.Base;
+
+namespace Blazor.Diagrams.Core.Positions.Resizing
+{
+    public abstract class ResizerProvider : IPositionProvider
+    {
+        abstract public string? Class { get; }
+
+        protected Size? OriginalSize { get; set; }
+        protected Point? OriginalPosition { get; set; }
+        protected double? LastClientX { get; set; }
+        protected double? LastClientY { get; set; }
+        protected NodeModel? NodeModel { get; set; }
+        protected Diagram? Diagram { get; set; }
+        private double _totalMovedX = 0;
+        private double _totalMovedY = 0;
+
+        abstract public bool ShouldChangeXPositionOnResize { get; }
+        abstract public bool ShouldChangeYPositionOnResize { get; }
+        ///<summary> Controls whether the totalMovedX should be added or subtracted </summary>
+        abstract public bool ShouldAddTotalMovedX { get; }
+        ///<summary> Controls whether the totalMovedY should be added or subtracted </summary>
+        abstract public bool ShouldAddTotalMovedY { get; }
+
+        abstract public Point? GetPosition(Model model);
+
+        virtual public (Size size, Point position) CalculateNewSizeAndPosition(double deltaX, double deltaY)
+        {
+            _totalMovedX += deltaX;
+            _totalMovedY += deltaY;
+
+            var width = OriginalSize!.Width + (ShouldAddTotalMovedX ? _totalMovedX : -_totalMovedX) / Diagram!.Zoom;
+            var height = OriginalSize.Height + (ShouldAddTotalMovedY ? _totalMovedY : -_totalMovedY) / Diagram!.Zoom;
+
+            var positionX = OriginalPosition!.X + (ShouldChangeXPositionOnResize ? _totalMovedX : 0) / Diagram!.Zoom;
+            var positionY = OriginalPosition.Y + (ShouldChangeYPositionOnResize ? _totalMovedY : 0) / Diagram!.Zoom;
+
+            if (width < NodeModel!.MinimumDimensions.Width)
+            {
+                width = NodeModel.MinimumDimensions.Width;
+                positionX = NodeModel.Position.X;
+            }
+            if (height < NodeModel.MinimumDimensions.Height)
+            {
+                height = NodeModel.MinimumDimensions.Height;
+                positionY = NodeModel.Position.Y;
+            }
+
+            return (new Size(width, height), new Point(positionX, positionY));
+        }
+
+        virtual public void SetSizeAndPosition(Size size, Point position)
+        {
+            NodeModel!.SetPosition(position.X, position.Y);
+            NodeModel.SetSize(size.Width, size.Height);
+        }
+
+        virtual public void OnResizeStart(Diagram diagram, Model model, PointerEventArgs e)
+        {
+            if (model is NodeModel nodeModel)
+            {
+                LastClientX = e.ClientX;
+                LastClientY = e.ClientY;
+                OriginalPosition = new Point(nodeModel.Position.X, nodeModel.Position.Y);
+                OriginalSize = nodeModel.Size;
+                this.NodeModel = nodeModel;
+                Diagram = diagram;
+            }
+        }
+
+        virtual public void OnPointerMove(Model? model, PointerEventArgs e)
+        {
+            if (OriginalSize is null || OriginalPosition is null || NodeModel is null || Diagram is null)
+            {
+                return;
+            }
+
+            var deltaX = (e.ClientX - LastClientX!.Value);
+            var deltaY = (e.ClientY - LastClientY!.Value);
+
+            LastClientX = e.ClientX;
+            LastClientY = e.ClientY;
+
+            var result = CalculateNewSizeAndPosition(deltaX, deltaY);
+            SetSizeAndPosition(result.size, result.position);
+        }
+
+        virtual public void OnResizeEnd(Model? model, PointerEventArgs args)
+        {
+            NodeModel?.TriggerSizeChanged();
+            OriginalSize = null;
+            OriginalPosition = null;
+            NodeModel = null;
+            _totalMovedX = 0;
+            _totalMovedY = 0;
+            LastClientX = null;
+            LastClientY = null;
+            Diagram = null;
+        }
+    }
+}
--- src/Blazor.Diagrams.Core/Positions/Resizing/TopRightResizerProvider.cs
+++ src/Blazor.Diagrams.Core/Positions/Resizing/TopRightResizerProvider.cs
@@ -1,77 +1,24 @@
-﻿using Blazor.Diagrams.Core.Events;
-using Blazor.Diagrams.Core.Geometry;
+﻿using Blazor.Diagrams.Core.Geometry;
 using Blazor.Diagrams.Core.Models;
 using Blazor.Diagrams.Core.Models.Base;
 
 namespace Blazor.Diagrams.Core.Positions.Resizing
 {
-    public class TopRightResizerProvider : IResizerProvider
+    public class TopRightResizerProvider : ResizerProvider
     {
-        public string? Class => "topright";
+        override public string? Class => "topright";
+        override public bool ShouldChangeXPositionOnResize => false;
+        override public bool ShouldChangeYPositionOnResize => true;
+        override public bool ShouldAddTotalMovedX => true;
+        override public bool ShouldAddTotalMovedY => false;
 
-        private Size? _originalSize;
-        private Point? _originalPosition;
-        private Point? _originalMousePosition;
-        private NodeModel? _nodeModel;
-        private Diagram? _diagram;
-
-        public Point? GetPosition(Model model)
+        public override Point? GetPosition(Model model)
         {
             if (model is NodeModel nodeModel && nodeModel.Size is not null)
             {
                 return new Point(nodeModel.Position.X + nodeModel.Size.Width + 5, nodeModel.Position.Y - 5);
             }
             return null;
-        }
-
-        public void OnResizeStart(Diagram diagram, Model model, PointerEventArgs eventArgs)
-        {
-            if (model is NodeModel nodeModel)
-            {
-                _originalPosition = new Point(nodeModel.Position.X, nodeModel.Position.Y);
-                _originalMousePosition = new Point(eventArgs.ClientX, eventArgs.ClientY);
-                _originalSize = nodeModel.Size;
-                _nodeModel = nodeModel;
-                _diagram = diagram;
-            }
-        }
-
-        public void OnPointerMove(Model? model, PointerEventArgs args)
-        {
-            if (_originalSize is null || _originalPosition is null || _originalMousePosition is null || _nodeModel is null || _diagram is null)
-            {
-                return;
-            }
-
-            var height = _originalSize.Height - (args.ClientY - _originalMousePosition.Y) / _diagram.Zoom;
-            var width = _originalSize.Width + (args.ClientX - _originalMousePosition.X) / _diagram.Zoom;
-
-            var positionX = _originalPosition.X;
-            var positionY = _originalPosition.Y + (args.ClientY - _originalMousePosition.Y) / _diagram.Zoom;
-
-            if (width < _nodeModel.MinimumDimensions.Width)
-            {
-                width = _nodeModel.MinimumDimensions.Width;
-                positionX = _nodeModel.Position.X;
-            }
-            if (height < _nodeModel.MinimumDimensions.Height)
-            {
-                height = _nodeModel.MinimumDimensions.Height;
-                positionY = _nodeModel.Position.Y;
-            }
-
-            _nodeModel.SetPosition(positionX, positionY);
-            _nodeModel.SetSize(width, height);
-        }
-
-        public void OnResizeEnd(Model? model, PointerEventArgs args)
-        {
-            _nodeModel?.TriggerSizeChanged();
-            _originalSize = null;
-            _originalPosition = null;
-            _originalMousePosition = null;
-            _nodeModel = null;
-            _diagram = null;
         }
 
     }

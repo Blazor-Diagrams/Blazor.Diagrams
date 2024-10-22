3c830c7fb2dd9e04cffdf8c5b013fc62f8f777d8
Mathias Andersson <mathias.andersson@telenorconnexion.com>
2024-10-22 12:12:55 +0200

    Added "Classes" that is then consumed by LinkWidget.

--- src/Blazor.Diagrams.Core/Models/LinkModel.cs
+++ src/Blazor.Diagrams.Core/Models/LinkModel.cs
@@ -23,5 +23,6 @@ public class LinkModel : BaseLinkModel
 
     public string? Color { get; set; }
     public string? SelectedColor { get; set; }
+    public string? Classes { get; set; }
     public double Width { get; set; } = 2;
 }

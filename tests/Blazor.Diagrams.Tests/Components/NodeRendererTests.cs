efd4f320d87683722344d6413fbb297f34c46b32
Haytam Zanid <haytam.zanid@gmail.com>
2024-12-03 12:42:17 +0100

    Avoid observing resizes when a node is invisible from the beginning

---
+++ tests/Blazor.Diagrams.Tests/Components/NodeRendererTests.cs
@@ -0,0 +1,36 @@
+﻿using Blazor.Diagrams.Components.Renderers;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using Bunit;
+using Xunit;
+
+namespace Blazor.Diagrams.Tests.Components;
+
+public class NodeRendererTests
+{
+
+    [Fact]
+    public void NodeRenderer_WhenNodeVisibleIsFalseAndIsFirstRender_ShouldNotObserveResizes()
+    {
+        // Arrange
+        JSRuntimeInvocationHandler call;
+        using (var ctx = new TestContext())
+        {
+            ctx.JSInterop.Setup<Rectangle>("ZBlazorDiagrams.getBoundingClientRect", _ => true);
+            call = ctx.JSInterop.SetupVoid("ZBlazorDiagrams.observe", _ => true).SetVoidResult();
+
+            // Act
+            var cut = ctx.RenderComponent<NodeRenderer>(p =>
+            {
+                p.Add(n => n.BlazorDiagram, new BlazorDiagram());
+                p.Add(n => n.Node, new NodeModel()
+                {
+                    Visible = false
+                });
+            });
+        }
+
+        // Assert
+        call.VerifyNotInvoke("ZBlazorDiagrams.observe");
+    }
+}
